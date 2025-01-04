4b6221ce7db51c82dc9643fb4d70cc83a1c771c1
izzydotexe <izzydotexe@github.io>
2025-01-04 15:48:58 -0500

    add env to next config

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,16 @@ const nextConfig = {
                 destination: `http://${BASE_URL}/auth/:path*`
             }
         ] : []
+    },
+    env: {
+        API_BASE_URL : process.env.UNITY_API_BASE_URI ?? 'http://localhost:3000',
+        STATUS_SITE_URL : process.env.STATUS_SITE ?? "http://localhost:3051/dashboard/3",
+        SITE_TITLE : process.env.SITE_TITLE ?? "ARC UNITY",
+        SITE_DESCRIPTION_META : process.env.SITE_DESCRIPTION_META?? "Arc unity dashboard dev version",
+        SITE_DESCRIPTION : process.env.SITE_DESCRIPTION?? `Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
+                    doloremque mollitia fugiat omnis! Porro facilis quo animi
+                    consequatur. Explicabo.`,
+        SITE_HERO_IMAGE : process.env.SITE_HERO_IMAGE?? "https://shadcnblocks.com/images/block/placeholder-1.svg"
     }
     
 };
